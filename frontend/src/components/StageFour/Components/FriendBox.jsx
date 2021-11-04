36aedcfc678023daa3d14e82d6d781034b161b9f
StephenTan12 <sttan@ucsd.edu>
2021-11-03 23:34:26 -0700

    started stage 4

---
+++ frontend/src/components/StageFour/Components/FriendBox.jsx
@@ -0,0 +1,9 @@
+const FriendBox = (props) => {
+  return (
+    <div>
+      <span>{props.friend}</span>
+    </div>
+  );
+};
+
+export default FriendBox;
--- frontend/src/components/StageFour/StageFour.jsx
+++ frontend/src/components/StageFour/StageFour.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+
+import FriendBox from "./Components/FriendBox";
+
+import globalStyles from "../../styles/styles.module.css";
+import mainStyles from "./StageFour.modules.css";
 
 const StageFour = (props) => {
+  const [query, changeQuery] = useState("");
+  const [friendData, changeFriendData] = useState([]);
+
+  useEffect(() => {
+    changeFriendData(["gello", "hello", "wow"]);
+  }, []);
+
   return (
-    <div>
-      <span>This is Stage Four!</span>
+    <div className={globalStyles.background}>
+      <input className={mainStyles.SearchBar} placeholder="Enter your Friend's Name" onChange={event => changeQuery(event.target.value)} />
+      <div className={mainStyles.MainBody}>
+        {friendData.filter(friend => {
+          if (query === '' || friend.toLowerCase().includes(query.toLowerCase())) {
+            return friend;
+          }
+        }).map( (friend, index) => 
+          <FriendBox friend={friend}/>
+        )}
+      </div>
     </div>
   );
 };
