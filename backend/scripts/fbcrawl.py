2453ebd217e15b32c4b682939d4e7f46bb7ab186
aabroukh <aabroukh@ucsd.edu>
2021-11-17 14:28:45 -0800

    cleaning up comments and functions

--- backend/scripts/fbcrawl.py
+++ backend/scripts/fbcrawl.py
@@ -1,28 +1,22 @@
-from time import sleep
-from pandas.core.indexes.base import Index
-from fbdriver import *
-from friend import Friend
-import urllib
-import random
-from collections import Counter
 import time
 import copy
-import pprint
-import json
 import pickle
 import pandas as pd
-from fbInferences import get_list_of_people, generate_inferences, generate_inferences_ranking
-from sql import insert_scraped_into_database, insert_inf_into_database
 import mysql.connector
 
+from fbdriver import *
+from fbscrape_helpers import *
+from fbInferences import get_list_of_people
+from sql import insert_scraped_into_database, insert_inf_into_database
+
+# connect to database and initialize schemas
 mydb = mysql.connector.connect(
   host="127.0.0.1",
   user="privacy_admin",
   password="kristenisthebest",
 )
-
 mycursor = mydb.cursor()
-sql_file = open("initialize_db.sql")
+sql_file = open("./backend/initialize_db.sql")
 sql_as_string = sql_file.read()
 sqlCommands = sql_as_string.split(';')
 for command in sqlCommands:
@@ -33,19 +27,20 @@ for command in sqlCommands:
 mydb.commit()
 sql_file.close()
 
+# connect to chromedriver for scraping
 path_to_chrome_driver = "/Users/aaron/opt/WebDriver/bin/chromedriver"
 username = "aaronbroukhim@aol.com"
 url = "https://mobile.facebook.com/home.php"
 
 driver = FBdriver(executable_path=path_to_chrome_driver)
 driver.set_page_load_timeout(60)
-# 5 should work on fast computers, increase if getting unable to locate element errors
-driver.implicitly_wait(10)
+driver.implicitly_wait(10) # 5 should work on fast computers, increase if getting unable to locate element errors
 driver.login(url, username) # type pw manually
 
-#keep track of time
+# keep track of time
 total_time = time.time()
 
+# dict to keep track of each element and the mutual friends that correspond to each
 category_groups_template = {
     "work": {
         "no_data": []
@@ -72,7 +67,7 @@ category_groups_template = {
 
 category_groups = copy.deepcopy(category_groups_template)
 
-# load in previously scraped data
+# load in previously scraped data from pickle file
 objects = []
 try:
     with (open("file.pkl", "rb")) as openfile:
@@ -84,7 +79,6 @@ try:
     print("Found pickle file")
 except:
     print("No pickle file")
-
 old_data = {}
 prev_friends_scraped = 0
 if objects:
@@ -93,67 +87,63 @@ if objects:
         prev_friends_scraped = old_data["count"]
     if "category_groups" in old_data:
         category_groups = old_data["category_groups"]
+    
 
-#scrape user info
-participant = get_participant_data(category_groups, driver)
+# scrape participant info if not in pickle
+if not old_data and not "friends" in old_data:
+    participant = get_participant_data(category_groups, driver)
+else:
+    participant = old_data["participant"]
 insert_scraped_into_database(participant, mydb, mycursor, True)
 
-#fetching all url paths to the user's friends' profiles
+# fetching all url paths to the user's friends' profiles
 if not old_data and not "friends" in old_data:
-    #change value to limit number of pages loaded for friends
+    # change value to limit number of pages loaded for friends
     friends = driver.full_friend_lookup_table(-1)
 else:
     friends = old_data["friends"]
 
-#table of times
+# table of times
 time_df = pd.DataFrame(columns=["mutual friends", "Word and ed", "Places lived", \
 "contact and basic info", "relationship and family", "total time"])
 
-#manual override if didnt scrape properly
+# manual override if didnt scrape properly
 #prev_friends_scraped = 250
 exception_list = []
 friends_with_most_data = []
 friends_with_least_data = []
 num_friends_scraped = 0
-num_to_scrape = 300 #len(friends) for all
-num_mutual_pages = -1 #-1 for all, otherwise a 8* will be number of friends scraped
-num_mutuals_inf = -1 #-1 for all, otherwise sets mutuals to make inferences on
+num_to_scrape = 300 # len(friends) for all
+num_mutual_pages = 1 # -1 for all, otherwise a 8* will be number of friends scraped
+num_mutuals_inf = -1 # -1 for all, otherwise sets mutuals to make inferences on
 time_df = pd.DataFrame(columns=[str(num_mutual_pages)+" pages", "Word and ed", \
 "Places lived", "contact and basic info", "friend total time"])
 print(f"friends scraped from pickle: {prev_friends_scraped}")
-#scrape friends
 for p, f in friends.items():
-#    try:
-    start_time = time.time()
-    #update data with old
-    if num_friends_scraped < prev_friends_scraped:
-        if not f.name or num_friends_scraped>=num_to_scrape:
-            continue
-        f = old_data["friends"][p]
-        # time_df.loc[len(time_df.index)] = f.time_array
-        print(num_friends_scraped)        
-        print(f.name)
-        print(f"Actual Mutual Friends: {f.numMutualFriends}")
-        print(f"Scraped Mutual Friends: {len(f.mutual_friends)}")
-        print("---------")
+     # start_time = time.time()
+    try:
+        # use backup pickle if present else scrape normally
+        if num_friends_scraped < prev_friends_scraped:
+            if not f.name or num_friends_scraped>=num_to_scrape:
+                continue
+            f = old_data["friends"][p]
+            # time_df.loc[len(time_df.index)] = f.time_array
+        else:
+            if num_friends_scraped >= num_to_scrape:
+                break
+            # get current friend data, mutual friends in batches of 8
+            f.time_array = scrape_friend_info(f, num_mutual_pages, category_groups, driver)
+            if not f.time_array:
+                driver.get("http://facebook.com")
+                f.name = ""
+                continue
+            populate_category_groups_funct(f, category_groups)
+
         num_friends_scraped+=1
-
-        # friends_with_least_data.append([f.url, f.percent_total_complete, f.numMutualFriends])
-        # friends_with_most_data.append([f.url, f.percent_total_complete, f.numMutualFriends])
-        # if len(friends_with_least_data) > 5:
-        #     friends_with_least_data = sorted(friends_with_least_data, key=lambda ele: (ele[1], -ele[2]))
-        #     friends_with_least_data.pop(-1)
-
-        # if len(friends_with_most_data) > 5:
-        #     friends_with_most_data = sorted(friends_with_most_data, reverse=True, key=lambda ele: (ele[1], ele[2]))
-        #     friends_with_most_data.pop(-1)
-
-        # print(f"least: {friends_with_least_data}")
-        # print(f"most: {friends_with_most_data}")
-        
         insert_scraped_into_database(f, mydb, mycursor)
-        #updating local data, breaking after number of friends achieved
-        # file = open("file.pkl","wb")
+
+        # # updating local data, breaking after number of friends achieved
+        # file = open(f"file_{num_friends_scraped}.pkl","wb")
         # formatted_data = {
         #     "count": num_friends_scraped,
         #     "friends": friends,
@@ -162,8 +152,15 @@ for p, f in friends.items():
         #     "time_df": time_df
         # }
         # pickle.dump(formatted_data, file)
+        # file.close()
+
+        print(num_friends_scraped)
+        print(f.name)
+        print(f"Actual Mutual Friends: {f.numMutualFriends}")
+        print(f"Scraped Mutual Friends: {len(f.mutual_friends)}")
 
         # insert event listener here for when we want to update inferences
+        # print("starting stage four, inserting inferences into database")
         # for url, friend in friends.items():
         #     category_frequency_data = copy.deepcopy(category_groups_template)
         #     for category, category_data in category_groups.items():
@@ -171,221 +168,30 @@ for p, f in friends.items():
         #             category_frequency_data[category][name] = list(set(get_list_of_people(friend.mutual_friends, participant.url, list_of_urls, num_mutuals_inf)))
         #     friend.inference_count = category_frequency_data
         #     insert_inf_into_database(friend, mydb, mycursor)
-        continue
-    if num_friends_scraped >= num_to_scrape:
+        # break
+
+        # print time and append time array to df
+        # print("friend total time: "+str(time.time()-start_time))
+        # f.time_array.append(float(time.time()-start_time))
+        # time_df.loc[len(time_df.index)] = f.time_array
+    except:
+        print(f"exception: {f.url}")
+        print("---------")
+        exception_list.append(f.url)
+
+        print("starting stage four, inserting inferences into database")
+        for url, friend in friends.items():
+            category_frequency_data = copy.deepcopy(category_groups_template)
+            for category, category_data in category_groups.items():
+                for name, list_of_urls in category_data.items():
+                    category_frequency_data[category][name] = list(set(get_list_of_people(friend.mutual_friends, participant.url, list_of_urls, num_mutuals_inf)))
+            friend.inference_count = category_frequency_data
+            insert_inf_into_database(friend, mydb, mycursor)
         break
-    #get current friend data, mutual friends in batches of 8
-    f.time_array = scrape_friend_info(f, num_mutual_pages, category_groups, driver)
-    if not f.time_array:
-        driver.get("http://facebook.com")
-        f.name = ""
-        continue
-    populate_category_groups_funct(f, category_groups)
 
-    # friends_with_least_data.append([f.url, f.percent_total_complete, f.numMutualFriends])
-    # friends_with_most_data.append([f.url, f.percent_total_complete, f.numMutualFriends])
-    # if len(friends_with_least_data) > 5:
-    #     friends_with_least_data = sorted(friends_with_least_data, key=lambda ele: (ele[1], -ele[2]))
-    #     friends_with_least_data.pop(-1)
-
-    # if len(friends_with_most_data) > 5:
-    #     friends_with_most_data = sorted(friends_with_most_data, reverse=True, key=lambda ele: (ele[1], ele[2]))
-    #     friends_with_most_data.pop(-1)
-
-    num_friends_scraped+=1
-    insert_scraped_into_database(f, mydb, mycursor)
-    # insert event listener here for when we want to update inferences
-    # for url, friend in friends.items():
-    #     category_frequency_data = copy.deepcopy(category_groups_template)
-    #     for category, category_data in category_groups.items():
-    #         for name, list_of_urls in category_data.items():
-    #             category_frequency_data[category][name] = list(set(get_list_of_people(friend.mutual_friends, participant.url, list_of_urls, num_mutuals_inf)))
-    #     friend.inference_count = category_frequency_data
-    #     insert_inf_into_database(friend, mydb, mycursor)
-
-    # print(f"least: {friends_with_least_data}")
-    # print(f"most: {friends_with_most_data}")
-    print(num_friends_scraped)
-    print(f.name)
-    print(f"Actual Mutual Friends: {f.numMutualFriends}")
-    print(f"Scraped Mutual Friends: {len(f.mutual_friends)}")
-    # print("---------")
-    #print time and append time array to df
-    #print("friend total time: "+str(time.time()-start_time))
-    #print("-------NEW FRIEND-------")
-    # f.time_array.append(float(time.time()-start_time))
-    # time_df.loc[len(time_df.index)] = f.time_array
-    #updating local data, breaking after number of friends achieved
-    # file = open("file.pkl","wb")
-    # formatted_data = {
-    #     "count": num_friends_scraped,
-    #     "friends": friends,
-    #     "participant": participant,
-    #     "category_groups": category_groups,
-    #     "time_df": time_df
-    # }
-    # pickle.dump(formatted_data, file)
-    # file.close()
-    # except:
-    #     print(f"exception: {f.url}")
-    #     print("---------")
-    #     exception_list.append(f.url)
-
-
-# dont need anything below this for actual experiment
-
-print("Begin Postprocessing")
-post_time = time.time()
 print(f"exception list: {exception_list}")
 print(f"number of friends scraped: {num_friends_scraped}")
 print("total runtime: "+str(time.time() - total_time))
-print("time averages: ")
-time_df.loc['mean'] = time_df.mean()
-print(time_df.loc['mean'])
-# time_df.to_csv(str(num_friends_scraped)+"friends_"+str(8*num_mutual_pages)+"mutuals.csv")
-
-
-'''
-look through each friends dcitionary
- - key/value pair is url/friend object pair
-    create a temp category groups object
- - go thru each category section and finding the length of the lists where the friend's 
-    mutual friends list intersects with the value list for that specific category data
- - make inferences after the calculating frequency of intersected data for each category
-
-for in friends
-    initialize temp category obj
-    for category in category groups
-        for dataentry in category
-            add mutual friend url to category
-'''
-inference_count_dict = {
-    "Not Scraped": 0,
-    "Has Ground Truth": {
-        "work": {
-            "Right": 0,
-            "Tie": 0,
-            "Wrong": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-            "Accuracy": 0,
-            "Sorted Accuracy": 0,
-
-        },
-        "college": {
-            "Right": 0,
-            "Tie": 0,
-            "Wrong": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-            "Accuracy": 0,
-            "Sorted Accuracy": 0,
-        },
-        "highschool": {
-            "Right": 0,
-            "Tie": 0,
-            "Wrong": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-            "Accuracy": 0,
-            "Sorted Accuracy": 0,
-        },
-        "cities": {
-            "Right": 0,
-            "Tie": 0,
-            "Wrong": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-            "Accuracy": 0,
-            "Sorted Accuracy": 0,
-        },
-        "religiousviews": {	
-            "Right": 0,
-            "Tie": 0,
-            "Wrong": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-            "Accuracy": 0,
-            "Sorted Accuracy": 0,
-        },
-        "politicalviews": {
-            "Right": 0,
-            "Tie": 0,
-            "Wrong": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-            "Accuracy": 0,
-            "Sorted Accuracy": 0,
-        },
-        "birthyear": {
-            "Right": 0,
-            "Tie": 0,
-            "Wrong": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-            "Accuracy": 0,
-            "Sorted Accuracy": 0,
-        },
-    },
-    "No Ground Truth": {
-        "work": {
-            "Tie": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-        },
-        "college": {
-            "Tie": 0,
-            "Below Threshold": 0,
-            "No Data": 0,
-        },
-        "highschool": {
-            "Tie": 0,
-            "Below Threshold": 0,
-            "No Data": 0
-        },
-        "cities": {
-            "Tie": 0,
-            "Below Threshold": 0,
-            "No Data": 0
-        },
-        "religiousviews": {	
-            "Tie": 0,
-            "Below Threshold": 0,
-            "No Data": 0
-        },
-        "politicalviews": {
-            "Tie": 0,
-            "Below Threshold": 0,
-            "No Data": 0
-        },
-        "birthyear": {
-            "Tie": 0,
-            "Below Threshold": 0,
-            "No Data": 0
-        },
-    },
-    "totals": {
-        "total right": 0,
-        "total wrong": 0,
-        "total tie": 0,
-        "total below threshold": 0,
-        "total no data": 0,
-    },
-    "percentages": {
-        "total right": 0,
-        "total wrong": 0,
-        "total tie": 0,
-        "total below threshold": 0,
-        "total no data": 0,
-        "right/rwt": 0,
-        "wrong/rwt": 0,
-        "tie/rwt": 0,
-    },
-    "avg confidence percentage": {
-        "right": 0,
-        "wrong": 0,
-    },
-}
 
 for url, friend in friends.items():
     category_frequency_data = copy.deepcopy(category_groups_template)
@@ -394,23 +200,3 @@ for url, friend in friends.items():
             category_frequency_data[category][name] = list(set(get_list_of_people(friend.mutual_friends, participant.url, list_of_urls, num_mutuals_inf)))
     friend.inference_count = category_frequency_data
     insert_inf_into_database(friend, mydb, mycursor)
-
-
-generate_inferences_ranking(friends, participant, inference_count_dict)
-# pprint.pprint(inference_count_dict)
-# with open(str(num_friends_scraped)+"friends_"+str(num_mutuals_inf)+"mutuals_inferences.json", "w") as outfile:
-#     json.dump(inference_count_dict, outfile)
-
-post_time_total = time.time()-post_time
-print(f"finished postprocessing{post_time_total}")
-
-inferences = []
-data_to_send = {
-    "friends": [f.attributes for f in friends.values()],
-    "inferences": inferences
-}
-
-querystring = urllib.parse.urlencode(data_to_send)
-driver.get(f'http://127.0.0.1:5000/go?{querystring}') # flask app
-
-driver.execute_script("alert('Finished!');")
---
+++ backend/scripts/fbscrape_helpers.py
@@ -0,0 +1,165 @@
+from friend import Friend
+
+def format_url(friend, sub_path):
+    places_url = f'https://facebook.com/{friend.url}'
+    if "profile.php" in friend.url:
+        return f'{places_url}&sk={sub_path}'
+    return f'{places_url}/{sub_path}'
+
+def extract_data(data, formatted_data):
+    try:
+        splitted_data = data.split("\n")
+        correct_data = [split_data for split_data in splitted_data if not "Shared " in split_data and not "Only " in split_data and not "Add " in split_data and not "Friends" in split_data]
+        if "gender" in formatted_data:
+            for i in range(1, len(correct_data)):
+                if "Gender" in correct_data[i] and not correct_data[i-1] in ["Male", "Female", "Agender", "Androgyne", "Androgynous", "Bigender", "Cis", "Cis Female", "Cis Male", "Cis Man", "Cis Woman", "Cisgender"]:
+                    correct_data.remove("Gender")
+                    break
+        #print(f"correct data: {correct_data}")  
+        for i in range(1, len(correct_data), 2):
+            category = correct_data[i+1].replace(" ", "").lower()
+            if category in formatted_data:
+                formatted_data[category] = correct_data[i]
+        return formatted_data
+    except:
+        return {}
+
+def generate_list_of_years(string_years, default_year=4):
+    '''
+    Case 1:
+        Date - Present
+    Case 2:
+        Date - Date
+    Case 3:
+        Class of Year
+    Case 4:
+        Starting on Date
+    Different Cases on Date Representation:
+    Case 1:
+        Month Day, Year
+    Case 2:
+        Month Year
+    Case 3:
+        Year
+    '''
+    list_of_years = []
+    start_year = 0
+    end_year = 0
+    if string_years == "NA":
+        return "NA"
+    # Case 1 and Case 2
+    if " - " in string_years:
+        separated_dates = string_years.split(" - ")
+        start_year = separated_dates[0]
+        # Case 1: Date Representation
+        if ", " in start_year:
+            start_year = int(start_year.split(", ")[1])
+        # Case 2: Date Representation
+        elif " " in start_year:
+            start_year = int(start_year.split(" ")[1])
+        # Case 3: Date Representation
+        else:
+            start_year = int(start_year)
+        # Case 2
+        if not "Present" in separated_dates[1]:
+            end_year = separated_dates[1]
+            if ", " in end_year:
+                end_year = int(end_year.split(", ")[1])
+            elif " " in end_year:
+                end_year = int(end_year.split(" ")[1])
+            else:
+                end_year = int(end_year)
+        else:
+            end_year = start_year + default_year
+    # Case 3
+    elif " of " in string_years:
+        end_year = int(string_years.split(" of ")[1])
+        start_year = end_year - default_year
+    # Case 4
+    elif " on " in string_years:
+        separated_date = string_years.split(" on ")[1]
+        separated_date = separated_date.split(", ")[1]
+        start_year = int(separated_date)
+        end_year = start_year + default_year
+    difference = end_year - start_year
+    for i in range(difference+1):
+        list_of_years.append(f'{start_year+i}')
+    return list_of_years
+
+def populate_category_groups(data, person_url, category_name, category_groups):
+    if data == "NA":
+        category_groups[category_name]["no_data"].append(person_url)
+        return
+    if not isinstance(data, list):
+        data = data
+        if data in category_groups[category_name]:
+            category_groups[category_name][data].append(person_url)
+        else:
+            category_groups[category_name][data] = [person_url]
+        return
+    for entry in data:
+        entry_name = entry
+        if isinstance(entry, dict):
+            entry_name = entry["title"]
+        entry_name = entry_name
+        if entry_name in category_groups[category_name]:
+            category_groups[category_name][entry_name].append(person_url)
+        else:
+            category_groups[category_name][entry_name] = [person_url]
+
+# scrape participants data and update their profile percent complete
+def get_participant_data(category_groups, driver):
+    participant = Friend(driver.participant_path)
+    participant.name = driver.scrape_participant_name(participant)
+    (count, participant.attributes["work"], participant.attributes["college"], participant.attributes["highschool"], participant.profile_picture_url, time) = driver.scrape_work_and_ed(participant)
+    participant.percent_complete+=count
+    populate_category_groups(participant.attributes["work"], participant.url, "work", category_groups)
+    populate_category_groups(participant.attributes["college"], participant.url, "college", category_groups)
+    populate_category_groups(participant.attributes["highschool"], participant.url, "highschool", category_groups)
+    (count, participant.attributes["places lived"], time) = driver.scrape_places_lived(participant)
+    participant.percent_complete+=count
+    populate_category_groups(participant.attributes["places lived"]["list_of_cities"], participant.url, "cities", category_groups)
+    (participant.percent_total_complete, count, participant.attributes["contact and basic"], time) = driver.scrape_contact_and_basic(participant)
+    participant.percent_complete+=count
+    populate_category_groups(participant.attributes["contact and basic"]["basic_info"]["religiousviews"], participant.url, "religiousviews", category_groups)
+    populate_category_groups(participant.attributes["contact and basic"]["basic_info"]["politicalviews"], participant.url, "politicalviews", category_groups)
+
+    participant.percent_complete+=count
+    participant.percent_total_complete+=participant.percent_complete
+    participant.percent_complete = round(participant.percent_complete/7, 3)
+    participant.percent_total_complete = round(participant.percent_total_complete/13, 3)
+    return participant
+
+def scrape_friend_info(f, num_mutual, category_groups, driver):
+    try:
+        time_array = []
+        f.name = driver.scrape_name(f)
+        time = driver.full_mutual_friend_list(f, num_mutual)
+        time_array.append(time)
+        (count, f.attributes["work"], f.attributes["college"], f.attributes["highschool"], f.profile_picture_url, time) = driver.scrape_work_and_ed(f)
+        time_array.append(time)
+        f.percent_complete+=count    
+        (count, f.attributes["places lived"], time) = driver.scrape_places_lived(f)
+        time_array.append(time)
+        f.percent_complete+=count    
+        (f.percent_total_complete, count, f.attributes["contact and basic"], time) = driver.scrape_contact_and_basic(f)
+        time_array.append(time)
+        #(tempCount, count, f.attributes["family and rel"], time) = driver.scrape_family_and_rel(f)
+        #time_array.append(time)
+        #f.percent_complete+=count
+        f.percent_total_complete+=f.percent_complete
+        #f.percent_total_complete+=tempCount
+        f.percent_complete = round(f.percent_complete/7, 3)
+        f.percent_total_complete = round(f.percent_total_complete/13, 3)
+        return time_array
+    except NoSuchElementException:
+        return None
+
+def populate_category_groups_funct(f, category_groups):
+    populate_category_groups(f.attributes["work"], f.url, "work", category_groups)
+    populate_category_groups(f.attributes["college"], f.url, "college", category_groups)
+    populate_category_groups(f.attributes["highschool"], f.url, "highschool", category_groups)
+    populate_category_groups(f.attributes["places lived"]["list_of_cities"], f.url, "cities", category_groups)
+    populate_category_groups(f.attributes["contact and basic"]["basic_info"]["religiousviews"], f.url, "religiousviews", category_groups)
+    populate_category_groups(f.attributes["contact and basic"]["basic_info"]["politicalviews"], f.url, "politicalviews", category_groups)
+    populate_category_groups(f.attributes["contact and basic"]["basic_info"]["birthyear"], f.url, "birthyear", category_groups)
