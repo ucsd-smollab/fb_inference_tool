c1781c114d90f5187b81e2f206cbfdff1914c0db
Mary Anne Smart <msmart@eng.ucsd.edu>
2021-11-18 18:42:44 -0800

    attempting stage 1, changing cursor behavior on buttons

--- frontend/src/components/StageOne/StageOne.jsx
+++ frontend/src/components/StageOne/StageOne.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
+import globalStyles from "../../styles/styles.module.css";
+import styles from "./StageOne.module.css";
+
 const StageOne = (props) => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className={globalStyles.background}>
+      <h1>What have you shared on your profile?</h1>
+      <div class={globalStyles.mainContent} >
+      <div class={styles.centered}>
+      <a href="https://www.facebook.com/profile" target="_blank" rel="noopener noreferrer">
+      <button class={styles.linktofb}>
+        View Your Info
+      </button>
+      </a>
+      </div>
+        <div className={globalStyles.ButtonContainer}>
+          <Link to="/StageTwoStepOne">
+            <button className={globalStyles.ButtonNav}>
+              Next
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
