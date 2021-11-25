52aaeacc9bd8a2a325bca1e17c16b258203727a5
aabroukh <aabroukh@ucsd.edu>
2021-11-24 17:09:24 -0800

    dropdown seems to be working, needs to be overlayed or can work as is, it automatically sets the selected profile to the last in the presenteed queries so that needs to be changed to be only on click

--- frontend/src/components/StageFour/Components/Dropdown.jsx
+++ frontend/src/components/StageFour/Components/Dropdown.jsx
@@ -6,7 +6,7 @@ const Dropdown = (props) => {
     }
 
     return (
-        <div>
+        <div className={globalStyles.mapContainer}>
             <div className={globalStyles.SearchContainer}>
                 <div className={globalStyles.SearchBar}>
                     <input style={{"zIndex": "1"}} type="text" placeholder="" onChange={handleChange}/>
@@ -15,15 +15,20 @@ const Dropdown = (props) => {
             <div className={globalStyles.SearchContainer}>
                 <div className={globalStyles.SearchBar}>
                     {Object.keys(props.friendSuggestions).map((key, index) => 
-                        <div style={{"marginTop": "-20px", "zIndex": "0"}} className={index%2 ? globalStyles.friendInfoHeaderEven : globalStyles.friendInfoHeader} onClick={props.onClick(key)}>
-                            <div className={globalStyles.headerContainer}>
-                            <span> 
-                                <img className={globalStyles.profilePicture} src={props.friendSuggestions[key][2]}/> 
-                            </span>
-                            <span>
-                                <div className={globalStyles.friendName}>{props.friendSuggestions[key][0]}</div>
-                                <div className={globalStyles.friendH4} style={{"justifySelf": "left"}}>{props.friendSuggestions[key][1]} Mutual Friends</div>
-                            </span>
+                        <div style={{"display":"flex", "width":"100%", "flexFlow":"row wrap"}}>
+                            {index===0 && (
+                            <div className={globalStyles.spacer}/>
+                            )}
+                            <div style={{"marginTop": "0px", "zIndex": "0", "height":"108px"}} className={index%2 ? globalStyles.friendInfoHeader : globalStyles.friendInfoHeaderOdd} onClick={props.onClick(key)}>
+                                <div className={globalStyles.headerContainer}>
+                                <span> 
+                                    <img style={{"width":"80px", "height":"80px", "zIndex":"2"}}className={globalStyles.profilePicture} src={props.friendSuggestions[key][2]}/> 
+                                </span>
+                                <span>
+                                    <div style={{"fontSize":"24px"}} className={globalStyles.friendName}>{props.friendSuggestions[key][0]}</div>
+                                    <div className={globalStyles.friendH5} style={{"justifySelf": "left"}}>{props.friendSuggestions[key][1]} Mutual Friends</div>
+                                </span>
+                                </div>
                             </div>
                         </div>
                     )}
